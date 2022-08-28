07e6ae8df0463f43ddd294944d97b264e74cf77c
Maxlego08 <maxlego08@sfr.fr>
2022-08-28 18:53:09 +0200

    :construction: Ajout du sass

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+                'node_modules/bootstrap-icons/font/bootstrap-icons.css'
+            ],
             refresh: true,
         }),
     ],
