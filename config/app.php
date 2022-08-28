8c9407740bd6029370208b531ea84f4346d4859c
Maxlego08 <maxlego08@sfr.fr>
2022-08-28 18:49:32 +0200

    :construction: Ajout de fortify

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        \App\Providers\FortifyServiceProvider::class,
     ],
 
     /*
