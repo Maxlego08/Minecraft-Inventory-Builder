bbbdcd10d5c55b4daafc7889217a7a4930b07f0f
Maxlego08 <maxlego08@sfr.fr>
2022-09-27 11:55:42 +0200

    :construction: Ajout de la validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,22 +15,6 @@
                     </div>
                 </div>
 
-                @if ($errors->any())
-                    <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <p>{{ __('Whoops! Something went wrong.') }}</p>
-
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                @endif
-
                 <div class="card">
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
@@ -41,25 +25,40 @@
                                 <div class="col-lg-8">
                                     <div class="mb-3">
                                         <label for="name" class="form-label ms-3">{{ __('Name') }}</label>
-                                        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                        @error('name')
+                                        <div id="password-error" class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="mb-3">
                                         <label for="email" class="form-label ms-3">{{ __('Email') }}</label>
-                                        <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                        @error('email')
+                                        <div id="password-error" class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="mb-3">
                                         <label for="password" class="form-label ms-3">{{ __('Password') }}</label>
-                                        <input type="password" class="form-control" id="password" name="password" required autocomplete="new-password">
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required autocomplete="new-password">
+                                        @error('password')
+                                        <div id="password-error" class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="mb-3">
                                         <label for="password_confirmation" class="form-label ms-3">{{ __('Confirm Password') }}</label>
-                                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required autocomplete="new-password">
+                                        <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation" name="password_confirmation" required autocomplete="new-password">
+                                        @error('password_confirmation')
+                                        <div id="password-error" class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <div class="my-4">
-                                        <input class="form-check-input" type="checkbox" name="terms" id="terms" required>
+                                        <input class="form-check-input @error('terms') is-invalid @enderror" type="checkbox" name="terms" id="terms" required>
                                         <label class="form-check-label" for="terms">
                                             J’accepte les <a href="#">conditions générales d’utilisation </a>et <a
                                                 href="#">la charte de confidentialité</a> du GroupeZ Marketplace.</label>
+                                        @error('terms')
+                                        <div id="password-error" class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                     <button type="submit" class="btn btn-primary rounded-4 d-block w-100 mt-5">{{ __('Register') }}
                                     </button>
