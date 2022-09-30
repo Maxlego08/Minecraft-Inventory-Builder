6d6b8f02c64fe793a88e3d0bd543559f96513ced
Maxlego08 <maxlego08@sfr.fr>
2022-09-30 18:56:03 +0200

    :construction: Ajout du view compose

---
+++ app/Http/View/Composers/AlertComposer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\View\Composers;
+
+use App\Models\Alert\AlertUser;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AlertComposer
+{
+
+    /**
+     * Bind data to the view.
+     *
+     * @param View $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+
+        if (!Auth::guest() && !$view->offsetExists('alertCount')) {
+            $alerts = AlertUser::where('user_id', Auth::user()->id)->whereNull('opened_at')->count();
+            $view->with('alertCount', $alerts);
+        }
+
+    }
+
+}
---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\View\Composers\AlertComposer;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class ViewServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('*', AlertComposer::class);
+    }
+
+}
