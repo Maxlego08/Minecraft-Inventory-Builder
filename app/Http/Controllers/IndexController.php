3460ede17d789d4f25e9fce5a47481d4d0cac548
Maxlego08 <maxlego08@sfr.fr>
2023-05-23 11:20:33 +0200

    :memo: Ajout de l'index controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Resource\Resource;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Cache;
+
+class IndexController extends Controller
+{
+    /**
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
+    public function index(): \Illuminate\Foundation\Application|View|Factory|Application
+    {
+        $resource = Cache::remember('resource:index', 86400, function (){
+            return Resource::first();
+        });
+        return view('home', [
+            'resource' => $resource,
+        ]);
+    }
+}
