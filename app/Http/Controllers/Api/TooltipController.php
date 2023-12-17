c9a88cd9389baf3809d83a11f372ae8c73f98d53
Maxlego08 <maxlego08@sfr.fr>
2023-12-17 21:21:44 +0100

    :memo: Fix tooltip

--- app/Http/Controllers/Api/TooltipController.php
+++ app/Http/Controllers/Api/TooltipController.php
@@ -12,60 +12,64 @@ class TooltipController extends Controller
 {
     public function tooltip(User $user): string
     {
-        // Préparation des variables PHP
-        $profilePhotoUrl = $user->getProfilePhotoUrlAttribute();
-        $userName = $user->name;
-        $displayNameAndLink = $user->displayNameAndLink(false);
-        $joinInfo = __('tooltip.join_at') . simple_date($user->created_at);
-        $conversationLink = $user->createConversation();
-        $conversationText = __('tooltip.conversation');
+        $userBannerStyle = '';
+        $userProfilePhotoUrl = htmlspecialchars($user->getProfilePhotoLargeUrlAttribute());
+        $userName = htmlspecialchars($user->name);
+        $userDisplayNameAndLink = $user->displayNameAndLink(false); // Assurez-vous que cette méthode échappe à son contenu interne
+        $userRoleIcon = $user->role->getRoleIcon(); // Assurez-vous que cette méthode échappe à son contenu interne
+        $userJoinDate = htmlspecialchars(simple_date($user->created_at));
+        $userResources = htmlspecialchars($user->getTooltipInformations()['resources'] ?? '');
+        $userPayments = htmlspecialchars($user->getTooltipInformations()['payments'] ?? '');
+        $userCreateConversationUrl = htmlspecialchars($user->createConversation());
+        $tooltipJoinAt = htmlspecialchars(__('tooltip.join_at'));
+        $tooltipConversation = htmlspecialchars(__('tooltip.conversation'));
+        $tooltipResources = htmlspecialchars(__('tooltip.resources'));
+        $tooltipPayments = htmlspecialchars(__('tooltip.payments'));
 
+        if (isset($user->banner_photo_path)) {
+            $userBannerUrl = htmlspecialchars($user->getBannerUrlAttribute());
+            $userBannerStyle = "style=\"background-image: url('$userBannerUrl'); background-repeat: no-repeat; background-size: cover; background-position: 0 0;\"";
+        }
 
-        $hasTooltipInformations = $user->hasTooltipInformations();
-        $resources = $hasTooltipInformations ? $user->getTooltipInformations()['resources'] : '';
-        $payments = $hasTooltipInformations ? $user->getTooltipInformations()['payments'] : '';
-        $resourcesLabel = __('tooltip.resources');
-        $paymentsLabel = __('tooltip.payments');
-
-        $htmlContent = <<<HTML
-<div class='user-tooltip'>
-    <div class="user-tooltip-header d-flex">
-        <div class="me-2">
-            <img src="{$profilePhotoUrl}" height="50" width="50" alt="{$userName}" class="rounded-2">
-        </div>
-        <div class="d-flex flex-column">
-            <span class="name">{$displayNameAndLink}</span>
-            <span class="join-info">{$joinInfo}</span>
-        </div>
-    </div>
-    <div class="user-tooltip-content">
-HTML;
-
-        if ($hasTooltipInformations) {
-            $htmlContent .= <<<HTML
-        <div class="d-flex justify-content-evenly user-tooltip-content-informations">
-            <div class="d-flex flex-column">
-                <span>{$resourcesLabel}</span>
-                <span class="text-center">{$resources}</span>
-            </div>
-            <div class="d-flex flex-column">
-                <span>{$paymentsLabel}</span>
-                <span class="text-center">{$payments}</span>
-            </div>
-        </div>
-        <hr>
+        $tooltipInformations = '';
+        if ($user->hasTooltipInformations()) {
+            $tooltipInformations = <<<HTML
+                    <div class="d-flex justify-content-evenly user-tooltip-content-informations">
+                        <div class="d-flex flex-column">
+                            <span>{$tooltipResources}</span>
+                            <span class="text-center">{$userResources}</span>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <span>{$tooltipPayments}</span>
+                            <span class="text-center">{$userPayments}</span>
+                        </div>
+                    </div>
+                    <hr>
 HTML;
         }
 
-        $htmlContent .= <<<HTML
-        <div class="user-tooltip-content-buttons">
-            <a class="conversation-button rounded-1" href="{$conversationLink}">{$conversationText}</a>
+        return <<<HTML
+        <div class='user-tooltip'>
+            <div class="user-tooltip-header d-flex" {$userBannerStyle}>
+                <div class="me-2">
+                    <img src="{$userProfilePhotoUrl}" height="75" width="75" alt="{$userName}" class="rounded-2">
+                </div>
+                <div class="d-flex flex-column">
+                    <span class="name">{$userDisplayNameAndLink}</span>
+                    <div class="pt-2 pb-2">
+                    {$userRoleIcon}
+                    </div>
+                    <span class="join-info">{$tooltipJoinAt}{$userJoinDate}</span>
+                </div>
+            </div>
+            <div class="user-tooltip-content">
+                {$tooltipInformations}
+                <div class="user-tooltip-content-buttons">
+                    <a class="conversation-button rounded-1" href="{$userCreateConversationUrl}">{$tooltipConversation}</a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 HTML;
-
-        return $htmlContent;
     }
 
     /**
