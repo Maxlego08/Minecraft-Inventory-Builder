c099e72ceb5f39a5a98f99abe127b1895f6abffa
Maxlego08 <maxlego08@sfr.fr>
2024-01-12 11:30:30 +0100

    :memo: Bug fix

--- resources/js/builder/components/inventory/InventoryCard.tsx
+++ resources/js/builder/components/inventory/InventoryCard.tsx
@@ -1,11 +1,42 @@
-import * as React from "react";
+import {useState} from "react";
 
 const InventoryCard = ({inventory}) => {
+
+    const [inventoryName, setInventoryName] = useState(inventory.file_name);
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedName, setEditedName] = useState(inventory.file_name);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleNameChange = (e) => {
+        setEditedName(e.target.value);
+    };
+
+    const handleFinishEdit = () => {
+        setIsEditing(false);
+        console.log("Finish edit name:", editedName)
+        console.log("Old Name:", inventory.file_name)
+
+        setInventoryName(editedName)
+    };
 
     return (
         <div className={'inventory-card'}>
             <div className={'inventory-card-header'}>
-                <span>{inventory.file_name}</span>
+                {isEditing ? (
+                    <input
+                        type="text"
+                        value={editedName}
+                        onChange={handleNameChange}
+                        onBlur={handleFinishEdit}
+                        className={'form-control rounded-0'}
+                        autoFocus
+                    />
+                ) : (
+                    <span onClick={handleEditClick}>{inventoryName}</span>
+                )}
             </div>
             <div className={'inventory-card-actions'}>
                 <div className={'inventory-card-actions-element'}>
