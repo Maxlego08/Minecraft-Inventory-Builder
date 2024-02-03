62217293b001a8234d173f3fa7d72408e76012d3
Maxlego08 <maxlego08@sfr.fr>
2024-02-03 12:24:44 +0100

    :memo: Edit

--- resources/views/builder/index.blade.php
+++ resources/views/builder/index.blade.php
@@ -8,7 +8,11 @@
 @section('title', 'Inventory Builder')
 
 @section('app')
+    @if(user()->isAdmin())
 <div id="builder">
 
 </div>
+    @else
+        <h1>You do not yet have access to the online editor, it is in development.</h1>
+    @endif
 @endsection
