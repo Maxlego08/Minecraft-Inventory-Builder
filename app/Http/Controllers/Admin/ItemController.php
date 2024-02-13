4ba98d5bf20c9d48da69d368c26b4928eaac28f5
Maxlego08 <maxlego08@sfr.fr>
2024-02-13 18:20:50 +0100

    :memo: Add information modal

---
+++ app/Http/Controllers/Admin/ItemController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Builder\Item;
+use App\Models\MinecraftVersion;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+
+class ItemController extends Controller
+{
+    public function updateItems(): RedirectResponse
+    {
+
+        $path = storage_path() . '/app/rendered.json';
+        $json = json_decode(file_get_contents($path), true);
+        $collections = collect();
+
+        foreach ($json as $value) {
+            $item = Item::where('css', $value['css'])->first();
+            if (isset($item)){
+                $version = MinecraftVersion::where('minecraft_version', $value['item']['version'] ?? 1.08)->first();
+                $item->update([
+                    'version_id' => $version?->id ?? 1,
+                    'minecraft_id' => $value['item']['id'] ?? null,
+                    'name' => $value['name'],
+                    'data' => $value['item']['data'] ?? 0,
+                    'material' => $value['item']['material'],
+                    'old_material' => $value['item']['oldMaterial'] ?? null,
+                    'max_stack_size' => $value['item']['max'] ?? 64,
+                ]);
+
+            } else {
+
+                $version = MinecraftVersion::where('minecraft_version', $value['item']['version'] ?? 1.08)->first();
+                Item::create([
+                    'version_id' => $version?->id ?? 1,
+                    'minecraft_id' => $value['item']['id'] ?? null,
+                    'name' => $value['name'],
+                    'data' => $value['item']['data'] ?? 0,
+                    'css' => $value['css'],
+                    'material' => $value['item']['material'],
+                    'old_material' => $value['item']['oldMaterial'] ?? null,
+                    'max_stack_size' => $value['item']['max'] ?? 64,
+                ]);
+            }
+        }
+
+
+        return Redirect::route('admin.index')->with('toast', createToast('success', 'Update of the items', 'You have just updated the items'));
+    }
+}
--- resources/views/builder/inventory.blade.php
+++ resources/views/builder/inventory.blade.php
@@ -23,6 +23,36 @@
         </div>
     </div>
     @include('builder.error')
+
+    <!-- Modal -->
+    <div class="modal fade modal-lg" id="monModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabel">Information !</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+                </div>
+                <div class="modal-body">
+                    <p>The inventory editor is currently in development.
+                        <br>
+                        You may encounter bugs during your use. It will also be missing features.
+                    </p>
+                    <p class="text-danger">Please report your bugs and ideas here: <a
+                            href="https://github.com/Maxlego08/Minecraft-Inventory-Builder/issues" target="_blank">https://github.com/Maxlego08/Minecraft-Inventory-Builder/issues</a>
+                    </p>
+                        <hr>
+                    <p>If you want to help the development of the site do not hesitate, your help will be welcome to improve the site</p>
+                    <hr>
+                    <div>Discord: <a href="https://discord.groupez.dev/" target="_blank">https://discord.groupez.dev/</a></div>
+                    <div>Support the project: <a href="{{ route('premium.index') }}">Upgrade my account</a></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 @push('footer-scripts')
