fc0a89196e789aea45e67e4345f32fa11d8a552f
Maxlego08 <maxlego08@sfr.fr>
2024-02-17 18:26:00 +0100

    :memo: Add information modal

--- resources/js/builder/inventory/components/modals/InformationModal.tsx
+++ resources/js/builder/inventory/components/modals/InformationModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Button } from 'react-bootstrap';
+import {Button, Modal} from 'react-bootstrap';
 
 const InformationModal = ({handleClose, show}) => {
 
@@ -8,8 +8,36 @@ const InformationModal = ({handleClose, show}) => {
                 <Modal.Title>Information</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p>Pour copier, utilisez <kbd>Ctrl</kbd> + <kbd>C</kbd>.</p>
-                <p>Pour coller, utilisez <kbd>Ctrl</kbd> + <kbd>V</kbd>.</p>
+                <p>
+                    <div><i className="bi bi-floppy me-1"></i> Saves the inventory.</div>
+                    <div><i className="bi bi-cloud-download me-1"></i> Allows you to download the YML configuration of the inventory.</div>
+                    <div><i className="bi bi-lock me-1"></i> Allows to lock the page, you will no longer scroll.</div>
+                    <div><i className="bi bi-info-lg me-1"></i> Displays the information of the inventory editor.</div>
+                </p>
+                <hr/>
+                <p>
+                    <div className={'h5 mb-1'}>Pickup an item</div>
+                    <div><kbd>Left Click</kbd> to take an item with an amount of 1.</div>
+                    <div><kbd>Shift</kbd> + <kbd>Left Click</kbd> to take an item with an amount 64.</div>
+                </p>
+                <hr/>
+                <p>
+                    <div className={'h5 mb-1'}>Place an item in the inventory.</div>
+                    <div><kbd>Right Click</kbd> to place the item in inventory.</div>
+                    <div><kbd>Left Click</kbd> to place a single item in the inventory.</div>
+                </p>
+                <hr/>
+                <p>
+                    <div className={'h5 mb-1'}>Interact with items in inventory</div>
+                    <div><kbd>Left Click</kbd> to select a slot. You must select a slot before you can move the item.</div>
+                    <div><kbd>Left Click</kbd> to take a whole item.</div>
+                    <div><kbd>Right Click</kbd> to take half the item.</div>
+                    <div><kbd>Shift</kbd> + <kbd>Left Click</kbd> to select multiple slots at the same time.</div>
+                    <div><kbd>Shift</kbd> +<kbd>Arrow Up</kbd> to move your selected slot up</div>
+                    <div><kbd>Shift</kbd> +<kbd>Arrow Down</kbd> to move your selected slot down</div>
+                    <div><kbd>Shift</kbd> +<kbd>Arrow Right</kbd> to move your selected slot right</div>
+                    <div><kbd>Shift</kbd> +<kbd>Arrow Left</kbd> to move your selected slot left</div>
+                </p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" size="sm" onClick={handleClose}>Close</Button>
