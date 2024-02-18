fdcf8b7e24586b9f62d8e3cf18c1c04786458698
Maxlego08 <maxlego08@sfr.fr>
2024-02-18 21:07:02 +0100

    :memo: Update

--- resources/js/builder/services/minecraftColor.js
+++ resources/js/builder/services/minecraftColor.js
@@ -15,20 +15,46 @@ const minecraftColorCodes = {
     '&d': '#FF55FF',     // Light Purple
     '&e': '#FFFF55',     // Yellow
     '&f': 'white',       // White
-    '&r': '',            // Reset
+    '&r': 'white',            // Reset
+};
+
+const miniMessageColorCode = {
+    'black': 'black',       // Black
+    'dark_blue': '#0000AA',     // Dark Blue
+    'dark_green': '#00AA00',     // Dark Green
+    'dark_aqua': '#00AAAA',     // Dark Aqua
+    'dark_red': '#AA0000',     // Dark Red
+    'dark_purple': '#AA00AA',     // Dark Purple
+    'gold': '#FFAA00',     // Gold
+    'gray': '#AAAAAA',     // Gray
+    'dark_gray': '#555555',     // Dark Gray
+    'blue': '#5555FF',     // Blue
+    'green': '#55FF55',     // Green
+    'aqua': '#55FFFF',     // Aqua
+    'red': '#FF5555',     // Red
+    'light_purple': '#FF55FF',     // Light Purple
+    'yellow': '#FFFF55',     // Yellow
+    'white': 'white',       // White
+    'reset': 'white',            // Reset
 };
 
 const translateMiniMessage = (text) => {
     // Translate MiniMessage tags into HTML
-    return text
-        .replace(/<([a-z]+|#[\da-fA-F]{6})>/g, (match, color) => `<span style="color:${color};">`)
+    return text.replace(/<([a-z]+|#[\da-fA-F]{6})>/g, (match, color) => `<span style="color:${miniMessageColorCode[color]};">`)
         .replace(/<\/[a-z]+>/g, () => '</span>');
 };
 
 const processMinecraftColorCodes = (text) => {
 
-    let processedText = translateMiniMessage(text);
-    return processedText.split(/(&[0-9a-fr]|#[\da-fA-F]{6})/)
+
+    text = text.replace(/&l/g, '<span style="font-weight: bold">');
+    text = text.replace(/&m/g, '<span style="text-decoration: line-through">');
+    text = text.replace(/&n/g, '<span style="text-decoration: underline">');
+    text = text.replace(/&o/g, '<span style="font-style: italic">');
+    text = text.replace(/&r/g, '<span style="font-style: normal; text-decoration: unset; font-weight: normal; color: white">');
+    text = text.replace('<reset>', '<span style="font-style: normal; text-decoration: unset; font-weight: normal; color: white">');
+
+    return translateMiniMessage(text.split(/(&[0-9a-fr]|#[\da-fA-F]{6})/)
         .reduce((acc, part) => {
             if (part.startsWith('&') && minecraftColorCodes.hasOwnProperty(part)) {
                 acc.currentColor = minecraftColorCodes[part];
@@ -45,8 +71,7 @@ const processMinecraftColorCodes = (text) => {
             }
 
             return acc;
-        }, {html: '', currentColor: ''})
-        .html;
+        }, {html: '', currentColor: ''}).html);
 };
 
 
