e74d08d0f967bdccc15307dc05a3caccdbd0a9be
Maxlego08 <maxlego08@sfr.fr>
2024-03-16 12:34:36 +0100

    :memo: Cache for heads

--- app/Http/Controllers/Builder/BuilderHeadController.php
+++ app/Http/Controllers/Builder/BuilderHeadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Builder;
 
 use App\Http\Controllers\Controller;
 use App\Models\Builder\Head;
+use Illuminate\Support\Facades\Cache;
 
 class BuilderHeadController extends Controller
 {
@@ -15,7 +16,18 @@ class BuilderHeadController extends Controller
      */
     public function search($search): bool|string
     {
-        $heads = Head::where('name', 'like', "%$search%")->orWhere('head_url', 'like', "%$search%")->limit(150)->get(['id', 'head_url', 'name', 'image_name', 'url_id']);
+        // Clé de cache unique pour cette requête de recherche spécifique
+        $cacheKey = 'search_heads_'.md5($search);
+
+        // Vérifie si les résultats existent déjà dans le cache
+        $heads = Cache::remember($cacheKey, 60*60*24, function () use ($search) { // Cache pour 1 jour
+            return Head::where('name', 'like', "%{$search}%")
+                ->orWhere('head_url', 'like', "%{$search}%")
+                ->limit(150)
+                ->get(['id', 'head_url', 'name', 'image_name', 'url_id']);
+        });
+
         return json_encode($heads);
     }
+
 }
