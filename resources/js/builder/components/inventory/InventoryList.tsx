a6cd2faa9c1d36ce22a5bc1c15aa644fd43b85e5
Maxlego08 <maxlego08@sfr.fr>
2024-04-09 18:49:42 +0200

    :memo: Delete old code

--- resources/js/builder/components/inventory/InventoryList.tsx
+++ resources/js/builder/components/inventory/InventoryList.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import InventoryHeader from './InventoryHeader'
 import Loader from "../utils/Loader";
 import api from '../../services/api';
 import InventoryCard from "./InventoryCard";
-import InventoryTable from "./InventoryTable";
+import {Form} from "react-bootstrap";
+import InventoryTableLine from "./InventoryTableLine";
 
 const InventoryList = ({folder = null}) => {
 
     const [inventories, setInventories] = useState(null);
+    const [data, setData] = useState([]);
+    const [sortConfig, setSortConfig] = useState({key: 'created_at', direction: 'ascending'});
+    const wrapperRef = useRef(null);
 
 
     useEffect(() => {
@@ -16,6 +20,10 @@ const InventoryList = ({folder = null}) => {
         fetchInventories();
     }, [folder]);
 
+    useEffect(() => {
+        setData(inventories ?? [])
+        // sortData('created_at')
+    }, [inventories])
 
     const fetchInventories = () => {
 
@@ -31,30 +39,50 @@ const InventoryList = ({folder = null}) => {
         });
     }
 
-    /**
-     *
-     * @param formData
-     */
-        // @ts-ignore
-    const handleCreateInventory = async (formData: FormData) => {
-            try {
-                const response = await api.createInventory(formData, folder.id)
-                api.displayToast(response)
-
-                if (response.data.result === 'success') {
-                    // @ts-ignore
-                    setInventories(currentInventories => [
-                        // @ts-ignore
-                        ...currentInventories,
-                        response.data.inventory
-                    ]);
-                }
-            } catch (error) {
-                console.error("Error deleting folder:", error);
-                // @ts-ignore
-                window.toast('error', 'Error !', `Error deleting folder: ${error}`, 5000)
-            }
+    const sortData = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
         }
+
+        const sortedData = [...data].sort((a, b) => {
+            if (a[key] < b[key]) {
+                return direction === 'ascending' ? -1 : 1;
+            }
+            if (a[key] > b[key]) {
+                return direction === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        });
+        setData(sortedData);
+        setSortConfig({key, direction});
+    };
+
+    const getClassName = (key) => `sort${sortConfig.key === key ? ' sort-current' : ''}`;
+
+    const getArrow = (key) => sortConfig.key !== key ? '' : sortConfig.direction === 'ascending' ?
+        <i className="bi bi-arrow-down"></i> : <i className="bi bi-arrow-up"></i>
+
+    // @ts-ignore
+    const handleCreateInventory = async (formData: FormData) => {
+        try {
+            const response = await api.createInventory(formData, folder.id)
+            api.displayToast(response)
+
+            if (response.data.result === 'success') {
+                // @ts-ignore
+                setInventories(currentInventories => [
+                    // @ts-ignore
+                    ...currentInventories,
+                    response.data.inventory
+                ]);
+            }
+        } catch (error) {
+            console.error("Error deleting folder:", error);
+            // @ts-ignore
+            window.toast('error', 'Error !', `Error deleting folder: ${error}`, 5000)
+        }
+    }
 
     const handleDeleteInventory = (inventoryId) => {
         api.deleteInventory(inventoryId).then(response => {
@@ -69,23 +97,59 @@ const InventoryList = ({folder = null}) => {
 
 
     return (
-        <div className={'inventories'}>
+        <div className={'inventories'} ref={wrapperRef}>
             <InventoryHeader createInventory={handleCreateInventory}/>
-            <InventoryTable inventories={inventories ?? [] } folder={folder} handleDeleteInventory={handleDeleteInventory}/>
-            {inventories ? (
-                <div className={'inventories-list'}>
-                    {// @ts-ignore
-                        inventories?.map((inventory, index) => (
-                            <InventoryCard key={`${index}-${folder.id}`} inventory={inventory}
-                                           handleDeleteInventory={handleDeleteInventory}/>
-                        ))}
+
+            <div className={'inventory-table'}>
+
+                <div className={'inventory-table-search'}>
+                    <div className={'search-icon'}>
+                        <i className="bi bi-filter"></i>
+                    </div>
+                    <div className={'search-bar'}>
+                        <Form.Control
+                            type="text"
+                            value={''}
+                            placeholder={'Filter'}
+                            onChange={(e) => {
+
+                            }}
+                            className={'rounded-0'}
+                        />
+                    </div>
                 </div>
-            ) : (
-                <Loader/>
-            )}
+                <hr/>
+                <div className={'inventory-table-content'}>
+                    <table className="table table-responsive table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th className={'select'}></th>
+                            <th className={getClassName('file_name')} onClick={() => sortData('file_name')}>File
+                                Name {getArrow('file_name')}</th>
+                            <th className={getClassName('name')}
+                                onClick={() => sortData('name')}>Name {getArrow('name')}</th>
+                            <th className={getClassName('size')}
+                                onClick={() => sortData('size')}>Size {getArrow('size')}</th>
+                            <th className={getClassName('inventory_visibility_id')}
+                                onClick={() => sortData('inventory_visibility_id')}>Visibility {getArrow('inventory_visibility_id')}</th>
+                            <th className={getClassName('created_at')} onClick={() => sortData('created_at')}>Created
+                                at {getArrow('created_at')}</th>
+                            <th className={getClassName('updated_at')} onClick={() => sortData('updated_at')}>Updated
+                                at {getArrow('updated_at')}</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data.map((inventory, index) => (
+                            <InventoryTableLine key={`${index}-${folder.id}`} inventory={inventory}
+                                                handleDeleteInventory={handleDeleteInventory}/>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
-
 }
 
 export default InventoryList
