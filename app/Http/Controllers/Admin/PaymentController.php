5b14758d0d8b2381def21179f3a14758f38525d5
Maxlego08 <maxlego08@sfr.fr>
2024-04-10 15:09:04 +0200

    :memo: Add unpaid delete method

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Payment\Payment;
+use App\Models\UserLog;
+use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class PaymentController extends Controller
 {
@@ -24,7 +28,7 @@ class PaymentController extends Controller
         $search = $request->input('search');
         $payments = Payment::select('payment_payments.*')->when($search, function (Builder $query, string $search) {
             return $query->leftJoin('users', 'payment_payments.user_id', '=', 'users.id')
-                ->where(function($query) use ($search) {
+                ->where(function ($query) use ($search) {
                     $query->where('payment_payments.user_id', 'like', "%{$search}%")
                         ->orWhere('payment_payments.payment_id', 'like', "%{$search}%")
                         ->orWhere('payment_payments.external_id', 'like', "%{$search}%")
@@ -53,5 +57,18 @@ class PaymentController extends Controller
         return view('admins.payments.show', [
             'payment' => $payment,
         ]);
+    }
+
+    /**
+     * Permet de supprimer les paiements non payés
+     *
+     * @return RedirectResponse
+     */
+    public function delete(): RedirectResponse
+    {
+        Payment::where('status', Payment::STATUS_UNPAID)->whereRaw('created_at <= ?', [Carbon::now()->subDay()])->delete();
+        userLog("Suppression des paiements non payés", UserLog::COLOR_DANGER, UserLog::ICON_TRASH);
+        return Redirect::route('admin.payments.index')->with('toast',
+            createToast('success', 'Succès', "Suppression des paiements non payés"));
     }
 }
