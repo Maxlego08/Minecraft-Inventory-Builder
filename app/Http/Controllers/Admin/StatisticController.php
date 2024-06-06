350bb5242ed80803ba80e459c1cd6291ea0fc5c9
Maxlego08 <maxlego08@sfr.fr>
2024-06-06 11:35:13 +0200

    :memo: Add more stats

--- app/Http/Controllers/Admin/StatisticController.php
+++ app/Http/Controllers/Admin/StatisticController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Builder\Inventory;
 use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -22,10 +23,16 @@ class StatisticController extends Controller
         $inventories = Inventory::count();
         $inventoriesWithButtons = Inventory::whereHas('buttons')->count();
         $verifiedUsers = User::whereNotNull('email_verified_at')->count();
+
+        $premiums = User::where('user_role_id', 3)->count();
+        $pros = User::where('user_role_id', 4)->count();
+
         return view('admins.statistic.index', [
             'inventories' => $inventories,
             'inventoriesWithButtons' => $inventoriesWithButtons,
             'verifiedUsers' => $verifiedUsers,
+            'premiums' => $premiums,
+            'pros' => $pros,
         ]);
     }
 }
--- resources/views/admins/statistic/index.blade.php
+++ resources/views/admins/statistic/index.blade.php
@@ -60,6 +60,40 @@
                 </div>
             </div>
 
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-secondary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Utilisateurs Premium
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $premiums }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-users fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Utilisateurs Pro
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $pros }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-users fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
