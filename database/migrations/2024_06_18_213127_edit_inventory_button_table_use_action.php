3664666c8934e52acecf422d5cfd481481997255
Maxlego08 <maxlego08@sfr.fr>
2025-03-20 19:11:52 +0100

    :memo: Create a jobs

--- database/migrations/2024_06_18_213127_edit_inventory_button_table_use_action.php
+++ database/migrations/2024_06_18_213127_edit_inventory_button_table_use_action.php
@@ -10,48 +10,10 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        $buttons = InventoryButton::all();
-        foreach ($buttons as $button) {
-            if ($button->messages) {
-                InventoryButtonAction::create([
-                    'inventory_button_id' => $button->id,
-                    'inventory_action_type_id' => 3,
-                    'data' => json_encode([
-                        'messages' => $button->messages,
-                        'minimessage' => true,
-                    ]),
-                ]);
-            }
-            if ($button->console_commands) {
-                InventoryButtonAction::create([
-                    'inventory_button_id' => $button->id,
-                    'inventory_action_type_id' => 2,
-                    'data' => json_encode([
-                        'commands' => $button->console_commands,
-                    ]),
-                ]);
-            }
-            if ($button->commands) {
-                InventoryButtonAction::create([
-                    'inventory_button_id' => $button->id,
-                    'inventory_action_type_id' => 1,
-                    'data' => json_encode([
-                        'commands' => $button->commands,
-                        'commandInChat' => false,
-                    ]),
-                ]);
-            }
-            if ($button->sound) {
-                InventoryButtonAction::create([
-                    'inventory_button_id' => $button->id,
-                    'inventory_action_type_id' => 9,
-                    'data' => json_encode([
-                        'sound' => $button->sound,
-                        'pitch' => $button->pitch ?? 1.0,
-                        'volume' => $button->volume ?? 1.0,
-                    ]),
-                ]);
-            }
+
+        $lastButton = InventoryButton::orderBy('id', 'desc')->first();
+        for ($i = 1; $i <= $lastButton->id; $i++) {
+            \App\Jobs\UpdateInventoryButtonJob::dispatch($i);
         }
     }
 
